7e16a171bdc45e2c6c2a3d57219cd743d7b10d91
rpedde <ron@pedde.com>
2013-07-26 16:26:33 +0000

    add ntp without dragging in swift-lite roles

--- roles/spc-starter-controller.rb
+++ roles/spc-starter-controller.rb
@@ -3,5 +3,5 @@ description "controller node for starter configuration"
 run_list(
          "recipe[swift-private-cloud::admin-server]",
          "recipe[swift-private-cloud::keystone]",
-         "role[swift-lite-ntp]"
+         "recipe[swift-lite::ntp-server]"
 )
