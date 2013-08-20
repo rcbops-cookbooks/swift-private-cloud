c4f54440147c6123e8dfeaaf5f05a5318861c1f0
Marcelo Martins <marcelo.martins@rackspace.com>
2013-08-20 18:41:05 -0500

    fixing ruby syntax

--- roles/spc-starter-saio.rb
+++ roles/spc-starter-saio.rb
@@ -2,6 +2,6 @@ name "spc-starter-saio"
 description "All-in-One storage node for starter configuration"
 run_list(
          "recipe[osops-utils::packages]",
-         "recipe[swift-private-cloud::storage-server]"
+         "recipe[swift-private-cloud::storage-server]",
          "recipe[swift-private-cloud::proxy-server]"
 )
