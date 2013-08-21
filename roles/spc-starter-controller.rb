46f4e210f63e9a445d70e1dd47894989d2d05589
rpedde <ron@pedde.com>
2013-08-21 22:28:37 +0000

    remove iptables on cent
    This might want revisited, or perhaps some attributes to control
    whether or not we want strong iptables.  For now, we'll just drop
    it.  If your swift network is a routable network, you have bigger
    issues already.
    
    Closes #50

--- roles/spc-starter-controller.rb
+++ roles/spc-starter-controller.rb
@@ -2,6 +2,7 @@ name "spc-starter-controller"
 description "controller node for starter configuration"
 run_list(
          "recipe[osops-utils::packages]",
+         "recipe[swift-private-cloud::iptables]",
          "recipe[swift-private-cloud::git-server]",
          "recipe[swift-private-cloud::admin-server]",
          "recipe[swift-private-cloud::keystone]",
--- roles/spc-starter-proxy.rb
+++ roles/spc-starter-proxy.rb
@@ -2,5 +2,6 @@ name "spc-starter-proxy"
 description "proxy node for starter configuration"
 run_list(
          "recipe[osops-utils::packages]",
+         "recipe[swift-private-cloud::iptables]",
          "recipe[swift-private-cloud::proxy-server]"
 )
