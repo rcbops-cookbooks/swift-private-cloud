0c6d8d550d964ef4b01fbac5058471709040cb64
rpedde <ron@pedde.com>
2013-08-28 19:28:55 +0000

    First pass of arbitrary configs
    closes #92

--- recipes/object-server.rb
+++ recipes/object-server.rb
@@ -22,13 +22,69 @@ include_recipe "swift-lite::object-server"
 
 common = node["swift-private-cloud"]["swift_common"]
 
+default_options = {
+  "DEFAULT" => {
+    "bind_ip" => "0.0.0.0",
+    "bind_port" => "6000",
+    "backlog" => "4096",
+    "workers" => "8",
+    "disable_fallocate" => "false",
+    "fallocate_reserve" => "0",
+    "user" => "swift",
+    "devices" => "/srv/node",
+    "swift_dir" => "/etc/swift"
+  },
+  "pipeline:main" => {
+    "pipeline" => "healthcheck recon object-server"
+  },
+  "app:object-server" => {
+    "use" => "egg:swift#object",
+    "log_facility" => "LOG_LOCAL1",
+    "mb_per_sync" => "64"
+  },
+  "filter:healthcheck" => {
+    "use" => "egg:swift#healthcheck"
+  },
+  "filter:recon" => {
+    "use" => "egg:swift#recon",
+    "log_facility" => "LOG_LOCAL2",
+    "recon_cache_path" => "/var/cache/swift",
+    "recon_lock_path" => "/var/lock/swift"
+  },
+  "object-replicator" => {
+    "log_facility" => "LOG_LOCAL2",
+    "concurrency" => "6",
+    "rsync_io_timeout" => "30",
+    "recon_enable" => "yes",
+    "recon_cache_path" => "/var/cache/swift"
+  },
+  "object-updater" => {
+    "log_facility" => "LOG_LOCAL2",
+    "concurrency" => "3",
+    "node_timeout" => "60",
+    "conn_timeout" => "5",
+    "recon_enable" => "yes",
+    "recon_cache_path" => "/var/cache/swift"
+  },
+  "object-auditor" => {
+    "log_facility" => "LOG_LOCAL2",
+    "recon_enable" => "yes",
+    "recon_cache_path" => "/var/cache/swift"
+  }
+}
+
+overrides = { "DEFAULT" => node["swift-private-cloud"]["swift_common"].select { |k, _| k.start_with?("log_statsd_") }}
+
+Chef::Log.error(overrides)
+
+if node["swift-private-cloud"]["object"] and node["swift-private-cloud"]["object"]["config"]
+  overrides = overrides.merge(node["swift-private-cloud"]["object"]["config"]) { |k, x, y| x.merge(y) }
+end
+
+Chef::Log.error(overrides)
+
 resources("template[/etc/swift/object-server.conf]").instance_exec do
   cookbook "swift-private-cloud"
   mode "0644"
-  variables variables.merge(
-    "log_statsd_host" => common["log_statsd_host"],
-    "log_statsd_port" => common["log_statsd_port"],
-    "log_statsd_default_sample_rate" => common["log_statsd_default_sample_rate"],
-    "log_statsd_sample_rate_factor" => common["log_statsd_sample_rate_factor"],
-    "log_status_metric_prefix" => common["log_statsd_metric_prefix"])
+  variables("config_options" => default_options.merge(overrides) { |k, x, y| x.merge(y) })
 end
