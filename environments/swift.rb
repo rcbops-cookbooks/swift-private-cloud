9da6e3b159e0b6d09905690e87fcfe4d61f28c37
William Kelly <the.william.kelly@gmail.com>
2013-09-16 17:23:06 -0500

    Updated swift.rb environment file

--- environments/swift.rb
+++ environments/swift.rb
@@ -2,30 +2,59 @@ name "swift"
 description "The swift environment"
 override_attributes(
   "vips" => {
+    # We currently do not set up a loadbalanced vip--it is assumed that the customer will configure this if he has multiple proxies.
     "swift-proxy" => "http://192.168.122.138:8080"
   },
+
+  # this block is only necessary if you are using the cookbooks to install keystone (rather than using an existing keystone)
   "mysql" => {
     "allow_remote_root" => true,
     "root_network_acl" => "%"
   },
+
+  # this block is only necessary if you are using the cookbooks to install keystone (rather than using an existing keystone)
   "nova" => {
   },
+
   "swift" => {
+    # this should be changed to something relatively random and of about the same length
     "swift_hash" => "-74b-Wq802mNM_h-_5-tr8A-"
   },
+
   "swift-private-cloud" => {
     "keystone" => {
-      "ops_password"=> "secrete"
+      # "ops_user" => "swiftops" (default)
+      "ops_password"=> "secrete",
+      "auth_password"=> "secrete",
+      "swift_admin_url"=> "", # should be set to the endpoint you desire
+      "swift_internal_url"=> "", # should be set to the endpoint you desire
+      "swift_public_url"=> "" # should be set to the endpoint you desire
+
+      # if using an existing keystone, define the following=>
+      # "keystone_admin_url"=> "http://ip=>port/v2.0"
+      # "keystone_internal_url"=> "http://ip=>port/v2.0"
+      # "keystone_public_url"=> "http://ip=>port/v2.0"
+    },
+
+    "dispersion"=> {
+       # the dispersion user is not automatically created.  You'll need to do the keystone needful.
+       # "dis_tenant"=> "dispersion" (default)
+       # "dis_user"=> "reporter" (default)
+      "dis_key"=> "notautomaticallycreated" # must be set
     },
     "network" => {
-      "management" => "192.168.122.0/24"
+      # this should be set to the network that will be used to run swift services
+      "management" => "192.168.122.0/24",
+      # this should be the same as management for now
+      "exnet" => "192.168.122.0/24"
     },
     "swift_common" => {
-      "swift_hash_prefix" => "staticprefix",
-      "swift_hash_suffix" => ""
+      "swift_hash_prefix" => "staticprefix", # this should be changed to something else
+      "swift_hash_suffix" => "" # this should be defined as blank
     }
   },
-  # this is for keystone and non swift tightly coupled cookbooks
+
+  # this block is only necessary if you are using the cookbooks to install keystone (rather than using an existing keystone)
   "osops_networks" => {
     "nova" => "192.168.122.0/24",
     "management" => "192.168.122.0/24",
