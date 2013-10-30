02970bcd79e3d6f7150e452df1150c10c5c24c07
Marcelo Martins <btorch@gmail.com>
2013-10-30 14:58:52 -0500

    Delete spc-starter-saio.rb

--- roles/spc-starter-saio.rb
+++
@@ -1,9 +0,0 @@
-name "spc-starter-saio"
-description "All-in-One storage node for starter configuration"
-run_list(
-  "recipe[swift-private-cloud::timezone]",
-  "recipe[osops-utils::packages]",
-  "recipe[swift-private-cloud::iptables]",
-  "recipe[swift-private-cloud::storage-server]",
-  "recipe[swift-private-cloud::proxy-server]"
-)
